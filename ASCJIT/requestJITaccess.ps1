dac983f36367938fb9e9581c60cacd0de484b792
Thomas Janetscheck <thj@live.de>
2017-12-08 11:57:36 +0100

    Request ASCJIT Access
    Request ASCJIT Access to RDP (Port 3389) for any VM in any eligible ARM subscription

--- ASCJIT/requestJITaccess.ps1
+++ ASCJIT/requestJITaccess.ps1
@@ -1,3 +1,5 @@
+### Request ASCJIT Access to RDP (Port 3389) for any VM in any eligible ARM subscription
+
 ### Login to your Azure AD Ressource Manager Account
 Login-AzureRmAccount
 ### In case you have several subscriptions select the proper one
@@ -5,6 +7,6 @@ Select-AzureRmSubscription (Get-AzureRMSubscription | Out-GridView -Title "Selec
 ### Select the Azure Resource Group and VM
 $ResourceGroupName = (Get-AzureRmResourceGroup | Out-GridView -Title "Select your Resource Group" -PassThru).ResourceGroupName
 $VMName = (Get-AzureRmVM -ResourceGroupName $ResourceGroupName  | Out-GridView -Title "Select your Virtual Machine" -PassThru).Name
-### Enter a time range greater than 6 minutes
+### Enter a time range greater than 6 minutes and request RDP access to the given VM
 $minutes = Read-Host "Enter the time range to open port 3389 on VM $VMName [minutes]"
 Invoke-ASCJITAccess -ResourceGroupName $ResourceGroupName -VM $VMName -Port 3389  -Minutes $minutes
