d020aa17ffdde7dc3244ac4fda783ac97cd8afcb
Thomas Janetscheck <thj@live.de>
2017-12-08 10:44:46 +0100

    Summary
    Use this code snippet to request Azure Security Center Just-in-Time Access (JIT) to a VM in any eligible ARM subscription.

---
+++ requestJITaccess.ps1
@@ -0,0 +1,10 @@
+### Login to your Azure AD Ressource Manager Account
+Login-AzureRmAccount
+### In case you have several subscriptions select the proper one
+Select-AzureRmSubscription (Get-AzureRMSubscription | Out-GridView -Title "Select your Azure Subscription" -PassThru)
+### Select the Azure Resource Group and VM
+$ResourceGroupName = (Get-AzureRmResourceGroup | Out-GridView -Title "Select your Resource Group" -PassThru).ResourceGroupName
+$VMName = (Get-AzureRmVM -ResourceGroupName $ResourceGroupName  | Out-GridView -Title "Select your Virtual Machine" -PassThru).Name
+### Enter a time range greater than 6 minutes
+$minutes = Read-Host "Enter the time range to open port 3389 on VM $VMName [minutes]"
+Invoke-ASCJITAccess -ResourceGroupName $ResourceGroupName -VM $VMName -Port 3389  -Minutes $minutes
