fd87bfdf03c6aa777da0b0a166fc6915f3972ae7
Thomas Janetscheck <thj@live.de>
2017-12-20 17:32:19 +0100

    debugged storage context creation

--- BlobLevelTiering/tierBlobsToArchive.ps1
+++ BlobLevelTiering/tierBlobsToArchive.ps1
@@ -1,7 +1,7 @@
 ### This code snippet is used to connect to an Azure Storage Account and tier all the blobs to Azure Archive Storage Tier.
 
 ### You need PowerShell module 4.4.0 or newer in order to be able to tier blobs to archive storage
-    install-module Azure -requiredVersion 4.4.0
+install-module Azure -requiredVersion 4.4.0
 
 ### Import Azure PowerShell module
     Import-Module Azure
@@ -18,10 +18,10 @@
     $ResourceGroupName = (Get-AzureRmResourceGroup | Out-GridView -Title "Select your Resource Group" -PassThru).ResourceGroupName
     $StorageAccount = Get-AzureRmStorageAccount -ResourceGroupName $ResourceGroupName | Out-GridView -Title "Select your Storage Account" -PassThru 
     $StorageAccountKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $StorageAccount.StorageAccountName)[0].Value
-    $containername = (Get-AzureStorageContainer -Context $context | Out-GridView -Title "Select your Storage Container" -PassThru).name
-
-### Create a storage context
+    
+    ### Create a storage context
     $context = New-AzureStorageContext -StorageAccountName $StorageAccount.StorageAccountName -StorageAccountKey $StorageAccountKey
+    $containername = (Get-AzureStorageContainer -Context $context | Out-GridView -Title "Select your Storage Container" -PassThru).name
     $blobs = Get-AzureStorageBlob -Container $containerName -Context $context
 
 ### Use the .Net client library API to change the access tier
