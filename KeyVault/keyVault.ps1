279cfe651702ba7ff353e7d86a35386c5360cf7e
Thomas Janetscheck <thj@live.de>
2018-06-14 00:46:04 +0200

    updated for Insider Dev Tour 2018 Cologne/Germany

--- KeyVault/keyVault.ps1
+++ KeyVault/keyVault.ps1
@@ -1,14 +1,22 @@
 # Login to Azure Subscription
-Login-AzureRMAccount
+  Login-AzureRMAccount
 
 # Create Azure KeyVault
-New-AzureRmKeyVault -VaultName "MyKeyVault" -ResourceGroupName "MyResourceGroup" -Location "WestEurope" -EnabledForDeployment -Sku standard
+  $vaultName = "InsiderDevTour2018CGN"
+  New-AzureRmKeyVault `
+    -VaultName $vaultName `
+    -ResourceGroupName (New-AzureRmResourceGroup -Name InsiderDevTour2018 -Location "WestEurope").ResourceGroupName`
+    -Location "WestEurope" `
+    -EnabledForDeployment `
+    -EnabledForTemplateDeployment `
+    -EnabledForDiskEncryption `
+    -Sku standard
 
 #Create a KeyVault Secret for a local admin password
-$password = read-host -assecurestring
-Set-AzureKeyVaultSecret -VaultName Mykeyvault -Name admin -SecretValue $password
+  $password = read-host -assecurestring
+  Set-AzureKeyVaultSecret -VaultName $vaultName -Name admin -SecretValue $password
 
 # build credentials with KeyVault Secret
-$LocalAdminUser = "myAdmin"
-$Secret = Get-AzureKeyVaultSecret -VaultName MyKeyVault -Name admin
-$Cred = [PSCredential]::new($LocalAdminUser, $Secret.SecretValue)
+  $LocalAdminUser = "myAdmin"
+  $Secret = Get-AzureKeyVaultSecret -VaultName $vaultName -Name admin
+  $Cred = [PSCredential]::new($LocalAdminUser, $Secret.SecretValue)
