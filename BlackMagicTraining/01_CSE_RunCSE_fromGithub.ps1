84dc73fda2ecad02bd2117a6d580bf6c96f4e79d
Thomas Janetscheck <thj@live.de>
2018-08-10 10:11:13 +0200

    fixed typo in variable name

--- BlackMagicTraining/01_CSE_RunCSE_fromGithub.ps1
+++ BlackMagicTraining/01_CSE_RunCSE_fromGithub.ps1
@@ -1,5 +1,9 @@
-### Run CSE on VM
-$locataion = "WestEurope"
+<#
+    You can use this code snippet without adding the CSE script file to a Storage Account.
+    The CSE is run directly from GitHub.
+#>
+
+$location = "WestEurope"
 $fileUri = "https://raw.githubusercontent.com/azureandbeyond/AzurePS/master/BlackMagicTraining/CustomScriptExtension.ps1"
 Set-AzureRmVMCustomScriptExtension -Name '<myCSE>' `
 -FileUri $fileUri `
