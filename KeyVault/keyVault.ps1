4aa1064d577b158a5d6c19f29b65e70b617348e9
Tom Janetscheck <32520935+azureandbeyond@users.noreply.github.com>
2019-03-08 11:16:01 +0100

    added KeyVaultAccessPolicy creation

--- KeyVault/keyVault.ps1
+++ KeyVault/keyVault.ps1
@@ -1,17 +1,26 @@
 # Login to Azure Subscription
   Login-AzAccount
 
+# Create Azure Resource Group
+$resourceGroupName = (New-AzResourceGroup -Name KeyVaultDemo -Location "WestEurope").ResourceGroupName
+
 # Create Azure KeyVault
   $vaultName = "IamaKeyVault"
   New-AzKeyVault `
     -VaultName $vaultName `
-    -ResourceGroupName (New-AzResourceGroup -Name KeyVaultDemo -Location "WestEurope").ResourceGroupName`
+    -ResourceGroupName $resourceGroupName`
     -Location "WestEurope" `
     -EnabledForDeployment `
     -EnabledForTemplateDeployment `
     -EnabledForDiskEncryption `
     -Sku standard
 
+# Give your user access rights to Azure KeyVault secrets
+Set-AzKeyVaultAccessPolicy -VaultName $vaultName `
+    -ResourceGroupName $resourceGroupName `
+    -UserPrincipalName (Get-AzContext).account.id `
+    -PermissionsToSecrets get,set
+    
 #Create a KeyVault Secret for a local admin password
   $password = read-host -assecurestring
   Set-AzKeyVaultSecret -VaultName $vaultName -Name admin -SecretValue $password
